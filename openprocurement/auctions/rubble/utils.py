efee01c11163d470c4f869decc86553bbddaf8a1
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-10-09 16:24:55 +0300

    Remove generate_rectificationPeriod

--- openprocurement/auctions/rubble/utils.py
+++ openprocurement/auctions/rubble/utils.py
@@ -140,19 +140,3 @@ def invalidate_bids_data(auction):
     for bid in auction.bids:
         setattr(bid, "status", "invalid")
     auction.rectificationPeriod.invalidationDate = get_now()
-
-
-def generate_rectificationPeriod(auction):
-    now = get_now()
-    if not auction.rectificationPeriod:
-        period = type(auction).rectificationPeriod.model_class()
-    period.startDate = period.startDate or now
-    if not period.endDate:
-        calculated_endDate = TZ.localize(
-            calculate_business_date(
-                auction.tenderPeriod.endDate,
-                -MINIMAL_PERIOD_FROM_RECTIFICATION_END,
-                auction).replace(tzinfo=None))
-        period.endDate = calculated_endDate if calculated_endDate > now else now
-    period.invalidationDate = None
-    return period
